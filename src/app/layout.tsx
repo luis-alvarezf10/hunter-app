acf429ae27001d31ccb658b7bb95bd076eb1c094
Luis Álvarez <luigi>
2026-03-04 13:46:17 -0400

    cambio de meta data en layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,8 +11,18 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Go Hunter",
   description: "Gestión inmobiliaria todas las estadísticas en la palma de tu mano",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "black-translucent", // O "default"
+  },
 };
 
+export const viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#ffffff" },
+    { media: "(prefers-color-scheme: dark)", color: "#060606" },
+  ],
+};
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -27,11 +37,16 @@ export default function RootLayout({
             __html: `
               (function() {
                 const theme = localStorage.getItem('theme');
-                if (theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-                  document.documentElement.classList.add('dark');
-                } else {
-                  document.documentElement.classList.remove('dark');
-                }
+                const isDark = theme === 'dark' || (!theme && window.matchMedia('(prefers-color-scheme: dark)').matches);
+        
+                // Aplicar clase al HTML
+                document.documentElement.classList.toggle('dark', isDark);
+                
+                // Aplicar color a la barra de estado inmediatamente
+                const meta = document.createElement('meta');
+                meta.name = "theme-color";
+                meta.content = isDark ? "#060606" : "#ffffff";
+                document.head.appendChild(meta);
               })();
             `,
           }}
