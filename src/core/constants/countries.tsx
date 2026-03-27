2cd7fd39e94d9c2e293632a32961347744d8a498
Luis Álvarez <124547619+luis-alvarezf10@users.noreply.github.com>
2026-03-27 16:53:42 -0400

    Actualizar countries.tsx

--- src/core/constants/countries.tsx
+++ src/core/constants/countries.tsx
@@ -2,5 +2,6 @@ export const COUNTRIES = [
   { code: "VE", name: "Venezuela", flag: "🇻🇪" },
   { code: "CO", name: "Colombia", flag: "🇨🇴" },
   { code: "MX", name: "México", flag: "🇲🇽" },
+  { code: "US", name: "Estado Unidos", flag: "🇺🇸" },
   // ... agrega todos los que necesites
 ];
